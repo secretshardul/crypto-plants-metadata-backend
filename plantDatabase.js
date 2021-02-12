341dd04f3a3917e6c39dd273bb4fdab3e7e4b5c8
shardul <shardul.aeer@somaiya.edu>
2021-02-13 01:35:56 +0530

    Add plants

--- plantDatabase.js
+++ plantDatabase.js
@@ -3,7 +3,12 @@ const plantDb = [
     'kjzl6cwe1jw14ayw4vbeha6l4hgtx4m60zwjgd48w4nu7kljx5km40excvxo0wo',
     'kjzl6cwe1jw1494gp0ln69bymuvjpyfjr2ipa0zaxd3syzufumumtva9khy32xk',
     'kjzl6cwe1jw14695fjmnideyoh1hfhbiddkq3f85x414bzklo9hrjug07fejalx',
-    'kjzl6cwe1jw14au4ejlkwpz2vo31x971fs6k72s56bx5gdn531x2v0xtcpx9vc7'
+    'kjzl6cwe1jw14au4ejlkwpz2vo31x971fs6k72s56bx5gdn531x2v0xtcpx9vc7',
+    'kjzl6cwe1jw1468ptz1ghsg4jljrwk61x4qvhd40peeomfejmf4ruwezm7xn8op',
+    'kjzl6cwe1jw147rpde2xzfafun1gs9tkhmpapcyaihxjkl08062ovsewons1wa4',
+    'kjzl6cwe1jw14ao89v7ls0cpf0cn5249rt0t5ax4soguqnv5gcvnxml0cn440o1',
+    'kjzl6cwe1jw149p21tfy52f52dmnb37efc3jch07w2op21ykhw3subajcwphhw1',
+    'kjzl6cwe1jw1481b9lg5xx1rakkciyw7zy6x71ifk1odpdd8sdgn45jo9z3hj44'
 ]
 
 module.exports = plantDb
